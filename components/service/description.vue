01feed5debd84d24bc9f8acb33d77867bdc20521
Nipaaaa1 <Frostone224@gmail.com>
2024-03-22 19:30:46 +0700

    feat: add services section and components

---
+++ components/service/description.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  description: string;
+}>();
+</script>
+
+<template>
+  <div class="space-y-3">
+    <h3 class="text-lg">{{ title }}</h3>
+    <p class="w-[20.0625rem] text-black/80">
+      {{ description }}
+    </p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
   >
     <Navigation />
     <Hero />
-    <main class="px-6 py-16">
+    <main class="flex flex-col gap-16 px-6 py-16">
       <Section>
         <SectionTitle name="01 - About Us">
           Crafting
@@ -15,6 +15,7 @@
         <img
           src="/assets/images/mobile/meeting.png"
           alt="People's meeting at work"
+          class="h-max w-full"
         />
         <h3 class="text-lg">
           “We're not just a studio, we're your steadfast ally”
@@ -32,6 +33,41 @@
           >
             How we Work <IconsArrowRight />
           </a>
+        </div>
+      </Section>
+      <Section>
+        <SectionTitle name="02 - Services">
+          Marais Studio Specialize in
+          <span class="text-primary"> websites </span> for a leading companies
+        </SectionTitle>
+
+        <div class="space-y-8">
+          <Service>
+            <ServiceImage
+              src="_nuxt/assets/images/mobile/laptop.png"
+              alt="A laptop on table"
+            />
+            <ServiceDescription
+              title="Web Design"
+              description="Putting People First in Web Design, we Harness Technology to
+                Elevate Human Experiences and Address Real-World Issues. As Your
+                Devoted Partner for the Long Haul, we Nurture Genuine
+                Cooperation Throughout the Process, Completely Devoted to Your
+                Achievement."
+            />
+          </Service>
+          <ServiceImage
+            src="_nuxt/assets/images/mobile/coding.png"
+            alt="A person coding on a laptop"
+          />
+          <ServiceDescription
+            title="Web Development"
+            description="Prioritizing People in Web Development, we Utilize Technology to
+                Enhance Human Experiences and Tackle Practical Challenges
+                Head-On. As Your Committed Long-Term Collaborator, we Foster
+                Authentic Collaboration Every Step of the Journey, Entirely
+                Dedicated to Your Success."
+          />
         </div>
       </Section>
     </main>
