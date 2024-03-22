63db8f4209a21f37287ac29fc117a43e06863b84
Nipaaaa1 <Frostone224@gmail.com>
2024-03-22 18:56:44 +0700

    feat: add about section and section components

---
+++ components/section/title.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <span>{{ name }}</span>
+    <h2 class="font-bebas-neue text-xl">
+      <slot />
+    </h2>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="text-md font-hanken-grotesk relative min-h-svh bg-white">
+  <div
+    class="text-md font-hanken-grotesk relative min-h-svh bg-white text-black"
+  >
     <Navigation />
     <Hero />
+    <main class="px-6 py-16">
+      <Section>
+        <SectionTitle name="01 - About Us">
+          Crafting
+          <span class="text-primary"> exceptional products </span>that resonate
+          with your audience, through strategic partnerships with leading
+          companies and brands.
+        </SectionTitle>
+        <img
+          src="/assets/images/mobile/meeting.png"
+          alt="People's meeting at work"
+        />
+        <h3 class="text-lg">
+          “We're not just a studio, we're your steadfast ally”
+        </h3>
+        <div class="space-y-3">
+          <p class="text-black/80">
+            Embracing human-centered design, we ensure technology serves
+            humanity, enriching experiences and solving real-world problems. As
+            your committed long-term partner, we foster genuine collaboration
+            from concept to product, fully invested in your success.
+          </p>
+          <a
+            href="#"
+            class="text-primary border-primary flex w-max items-center border-b"
+          >
+            How we Work <IconsArrowRight />
+          </a>
+        </div>
+      </Section>
+    </main>
   </div>
 </template>
