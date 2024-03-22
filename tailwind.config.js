3b2138a915089f1a38fcd9ed8c4ba34c0dff31d7
Nipaaaa1 <Frostone224@gmail.com>
2024-03-22 18:07:32 +0700

    style: add font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,70 @@ export default {
         "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
         "bebas-neue": ["Bebas Neue", "sans-serif"],
       },
+      fontSize: {
+        sm: [
+          "0.5625rem",
+          {
+            lineHeight: "120%",
+          },
+        ],
+        md: [
+          "0.875rem",
+          {
+            lineHeight: "120%",
+          },
+        ],
+        lg: [
+          "1.4375rem",
+          {
+            lineHeight: "120%",
+            fontWeight: "600",
+          },
+        ],
+        xl: [
+          "2.3125rem",
+          {
+            lineHeight: "120%",
+          },
+        ],
+        "2xl": [
+          "3.6875rem",
+          {
+            lineHeight: "100%",
+          },
+        ],
+        "desktop-sm": [
+          "0.625rem",
+          {
+            lineHeight: "120%",
+          },
+        ],
+        "desktop-md": [
+          "1rem",
+          {
+            lineHeight: "120%",
+          },
+        ],
+        "desktop-lg": [
+          "1.625rem",
+          {
+            lineHeight: "120%",
+            fontWeight: "600",
+          },
+        ],
+        "desktop-xl": [
+          "2.625rem",
+          {
+            lineHeight: "120%",
+          },
+        ],
+        "desktop-2xl": [
+          "4.250rem",
+          {
+            lineHeight: "100%",
+          },
+        ],
+      },
     },
   },
   plugins: [],
