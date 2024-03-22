e76732c12dae9a35c2974e42208b9dec2fe4bb06
Nipaaaa1 <Frostone224@gmail.com>
2024-03-22 18:06:27 +0700

    feat: add Hero

---
+++ components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="relative">
+    <img
+      src="/assets/images/mobile/hero.png"
+      alt="A man using laptop"
+      class="h-max w-full"
+    />
+    <div class="absolute top-0 flex size-full items-center p-6">
+      <div class="flex -translate-y-[7.3125rem] flex-col gap-6">
+        <h1 class="font-bebas-neue text-2xl text-white">
+          You Deserve a Great Website
+        </h1>
+        <div class="space-y-2">
+          <p class="w-[20.0625rem] text-white/80">
+            One of the most impressive aspects of MARAIS's work is their ability
+            to capture the essence of our brand and translate it into a visually
+            stunning digital platform.
+          </p>
+          <div class="flex flex-col text-white">
+            <span>Adam Smith</span>
+            <span class="text-sm">CEO of Hubble</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
