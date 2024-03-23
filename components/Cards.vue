2f3b82558727298bd9a1d9a29774dbaee25614c9
Nipaaaa1 <Frostone224@gmail.com>
2024-03-23 16:56:19 +0700

    feat:add desktop version

--- components/Cards.vue
+++ components/Cards.vue
@@ -10,7 +10,9 @@ defineProps<{
     <div
       class="absolute bottom-0 z-10 flex h-24 w-full items-end bg-gradient-to-t from-black to-transparent p-5"
     >
-      <h2 class="font-bebas-neue text-xl text-white">{{ title }}</h2>
+      <h2 class="font-bebas-neue text-xl text-white md:text-desktop-xl">
+        {{ title }}
+      </h2>
     </div>
   </div>
 </template>
--- components/Works.vue
+++ components/Works.vue
@@ -4,33 +4,53 @@
       <span class="text-primary">Collaborating</span> with Purposeful Tech
       Visionaries
     </SectionTitle>
-    <div class="grid grid-cols-1 gap-3">
+    <div class="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-x-5 md:gap-y-6">
       <Cards title="wave">
         <img
           src="/public/images/mobile/wave.png"
           alt="wave's Website"
-          class="h-max w-full"
+          class="h-max w-full md:hidden"
+        />
+        <img
+          src="/public/images/desktop/wave.png"
+          alt="wave's Website"
+          class="hidden h-max w-full md:block"
         />
       </Cards>
       <Cards title="reflecta">
         <img
           src="/public/images/mobile/reflecta.png"
           alt="reflecta's Website"
-          class="h-max w-full"
+          class="h-max w-full md:hidden"
+        />
+        <img
+          src="/public/images/desktop/reflecta.png"
+          alt="reflecta's Website"
+          class="hidden h-max w-full md:block"
         />
       </Cards>
       <Cards title="solo">
         <img
           src="/public/images/mobile/solo.png"
           alt="solo's Website"
-          class="h-max w-full"
+          class="h-max w-full md:hidden"
+        />
+        <img
+          src="/public/images/desktop/solo.png"
+          alt="solo's Website"
+          class="hidden h-max w-full md:block"
         />
       </Cards>
       <Cards title="dappr">
         <img
           src="/public/images/mobile/dappr.png"
           alt="dappr's Website"
-          class="h-max w-full"
+          class="h-max w-full md:hidden"
+        />
+        <img
+          src="/public/images/desktop/dappr.png"
+          alt="dappr's Website"
+          class="hidden h-max w-full md:block"
         />
       </Cards>
     </div>
