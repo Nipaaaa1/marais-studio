b65fb4d6fe2f15539eb673b68d2429d99d7e839c
Nipaaaa1 <Frostone224@gmail.com>
2024-03-23 17:06:43 +0700

    feat:add desktop version

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
-  <footer class="w-full space-y-6 bg-black p-6">
-    <div class="flex items-center gap-3">
-      <span class="footer-title font-bebas-neue text-2xl">Marais Studio</span>
-      <div class="flex flex-col gap-2 text-white">
+  <footer class="px-desktop w-full space-y-6 bg-black p-6 md:space-y-12">
+    <div class="flex items-center gap-3 md:gap-6">
+      <span class="footer-title font-bebas-neue text-2xl md:text-desktop-2xl"
+        >Marais Studio</span
+      >
+      <div class="flex flex-col gap-2 text-white md:gap-3">
         <span>Zurich, Switzerland</span>
         <span class="flex items-center"
           >Back to Top <IconsArrowUp class="h-full w-max"
@@ -14,7 +16,7 @@
       <li>Services</li>
       <li>Works</li>
     </ul>
-    <div class="flex justify-between text-sm text-white">
+    <div class="flex justify-between text-sm text-white md:text-desktop-sm">
       <span>Copyright 2024</span>
       <span>Devan Sayidina</span>
     </div>
