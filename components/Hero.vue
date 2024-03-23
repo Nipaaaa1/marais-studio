6a8aa66c4691c9f3e73d57199ebdcc68ef446ddb
Nipaaaa1 <Frostone224@gmail.com>
2024-03-23 16:19:12 +0700

    feat:add desktop version

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,14 +3,23 @@
     <img
       src="/public/images/mobile/hero.png"
       alt="A man using laptop"
-      class="h-max w-full"
+      class="h-max w-full md:hidden"
     />
-    <div class="absolute top-0 flex size-full items-center p-6">
-      <div class="flex -translate-y-[7.3125rem] flex-col gap-6">
-        <h1 class="font-bebas-neue text-2xl text-white">
+    <img
+      src="/public/images/desktop/hero.png"
+      alt="A man using laptop"
+      class="hidden h-max w-full md:block"
+    />
+    <div
+      class="md:px-desktop absolute top-0 flex size-full items-center p-6 md:items-start"
+    >
+      <div
+        class="flex w-full -translate-y-[7.3125rem] flex-col gap-6 md:translate-y-[23.1875rem]"
+      >
+        <h1 class="font-bebas-neue text-2xl text-white md:text-desktop-2xl">
           You Deserve a Great Website
         </h1>
-        <div class="space-y-2">
+        <div class="space-y-2 md:space-y-6 md:self-end">
           <p class="w-[20.0625rem] text-white/80">
             One of the most impressive aspects of MARAIS's work is their ability
             to capture the essence of our brand and translate it into a visually
@@ -18,7 +27,7 @@
           </p>
           <div class="flex flex-col text-white">
             <span>Adam Smith</span>
-            <span class="text-sm">CEO of Hubble</span>
+            <span class="text-sm md:text-desktop-sm">CEO of Hubble</span>
           </div>
         </div>
       </div>
