bc5448ffe1686ef223816b9aaba0033fb668b543
Nipaaaa1 <Frostone224@gmail.com>
2024-03-23 17:22:29 +0700

    feat:add SEO

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Marais Studio",
+  meta: [
+    {
+      name: "description",
+      content: "Marais is a Modern Web Studio for your Bussiness",
+    },
+  ],
+});
+</script>
+
 <template>
   <div
     class="relative min-h-svh bg-white font-hanken-grotesk text-md text-black md:text-desktop-md"
@@ -5,7 +17,7 @@
     <Navigation />
     <Hero />
     <main
-      class="md:px-desktop flex flex-col gap-16 px-6 py-16 md:gap-32 md:py-32"
+      class="flex flex-col gap-16 px-6 py-16 md:gap-32 md:px-desktop md:py-32"
     >
       <About />
       <Services />
